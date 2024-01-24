2921647b144f34d511a1e40c2702534806a89730
BoneInscri <20051124@hdu.edu.cn>
2024-01-24 22:05:39 +0800

    finish exercise 2.1

---
+++ SICP/code/chapter-2/2-01.scm
@@ -0,0 +1,46 @@
+#lang sicp
+
+(define (make-rat n d)
+  (define (gcd a b)
+    (if (= b 0)
+        a
+        (gcd b (remainder a b))))
+  (define s
+    (if (or (and (> 0 n) (> 0 d)) (and (< 0 n) (< 0 d))) 1 -1))
+  (let ((g (gcd n d)))
+      (cons (* s (/ n g)) (abs (/ d g))))
+)
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define x (make-rat 1 2))
+(define y (make-rat 1 -2))
+(define z (make-rat -2 -6))
+(print-rat x)
+(print-rat y)
+(print-rat z)
